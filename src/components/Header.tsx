196496c75dc8585da859cd7fc03573c494e8122f
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-29 20:27:47 -0300

    Criação do header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Navbar } from './Navbar';
+
+export const Header: React.FC = () => {
+    return (
+        <header >
+            <h1 >ConectaHC</h1>
+            <Navbar />
+        </header>
+    );
+};
