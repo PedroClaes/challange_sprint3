1721fa3098268d56d813b5c3b17de1f960b95e68
PedroClaes <pedrogabrielclaes@gmail.com>
2025-09-30 14:19:03 -0300

    Criação da página contato e chamando o component forms

---
+++ src/pages/Contato.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
+import { ContactForm } from '../components/ContatoForm';
+export const ContatoPage: React.FC = () => {
+    return (
+        <>
+            <Header />
+            <main className="flex-grow container mx-auto p-4">
+                <section>
+                    <h2 className="text-2xl font-bold text-center my-5" id="title">Fale Conosco</h2>
+                    <ContactForm /> {/* Use o componente de formulário */}
+                </section>
+            </main>
+            <Footer />
+        </>
+    );
+};
