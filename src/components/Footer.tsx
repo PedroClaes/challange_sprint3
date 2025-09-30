3ced36977331f4cfc86e20f4197a97fa5f0dba4b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-09-29 21:03:00 -0300

    Criando o footer da página

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+    return (
+        <footer className="bg-primary text-white p-5 text-center mt-auto">
+            <p>&copy; 2025 ConectaHC</p>
+        </footer>
+    );
+};
